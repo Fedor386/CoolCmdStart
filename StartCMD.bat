0dc026c00d73de751bb589ede9d7183fa2797350
Fedor386 <fedorkorobov386@gmail.com>
2025-11-15 13:36:59 +0300

    Add files via upload

---
+++ StartCMD.bat
@@ -0,0 +1,59 @@
+@echo off
+chcp 65001 >nul
+color 2
+timeout /NoBreak /t 1 >nul
+echo ██████  ██      ██  ██████        ██      ██  ██      ██  ██████
+timeout /NoBreak /t 1 >nul
+echo ██      ████  ████  ██    ██      ██          ████    ██  ██    
+timeout /NoBreak /t 1 >nul
+echo ██      ██  ██  ██  ██    ██      ██      ██  ██  ██  ██  ██████
+timeout /NoBreak /t 1 >nul
+echo ██      ██      ██  ██    ██      ██      ██  ██    ████  ██    
+timeout /NoBreak /t 1 >nul
+echo ██████  ██      ██  ██████        ██████  ██  ██      ██  ██████
+timeout /NoBreak /t 1 >nul
+echo.
+timeout /NoBreak /t 1 >nul
+echo ██████  ██████████    ██    ████    ██████████  ██  ██      ██  ████████
+timeout /NoBreak /t 1 >nul
+echo ██          ██      ██  ██  ██  ██      ██          ████    ██  ██
+timeout /NoBreak /t 1 >nul
+echo ██████      ██      ██████  ████        ██      ██  ██  ██  ██  ██  ████
+timeout /NoBreak /t 1 >nul
+echo     ██      ██      ██  ██  ██  ██      ██      ██  ██    ████  ██    ██
+timeout /NoBreak /t 1 >nul
+echo ██████      ██      ██  ██  ██  ██      ██      ██  ██      ██  ████████
+timeout /NoBreak /t 1 >nul
+echo.
+timeout /NoBreak /t 1 >nul
+echo.
+timeout /NoBreak /t 1 >nul
+echo                             ██  ██  ██
+timeout /NoBreak /t 5 >nul
+cls
+echo ████████  ████████  ████████  ████████  ████████        ██    ██      ██  ██      ██      ██    ██  ██████  ██      ██
+timeout /NoBreak /t 1 >nul
+echo ██    ██  ██    ██  ██        ██        ██            ██  ██  ████    ██    ██  ██        ██  ██    ██        ██  ██  
+timeout /NoBreak /t 1 >nul
+echo ████████  ████████  ████████  ████████  ████████      ██████  ██  ██  ██      ██          ████      ██████      ██    
+timeout /NoBreak /t 1 >nul
+echo ██        ██  ██    ██              ██        ██      ██  ██  ██    ████      ██          ██  ██    ██          ██  
+timeout /NoBreak /t 1 >nul
+echo ██        ██    ██  ████████  ████████  ████████      ██  ██  ██      ██      ██          ██    ██  ██████      ██   
+timeout /NoBreak /t 1 >nul
+echo.
+timeout /NoBreak /t 1 >nul
+echo ██████████  ████████      ██████  ████████  ██      ██  ██████████  ██  ██      ██  ██    ██  ██████
+timeout /NoBreak /t 1 >nul
+echo     ██      ██    ██      ██      ██    ██  ████    ██      ██          ████    ██  ██    ██  ██
+timeout /NoBreak /t 1 >nul
+echo     ██      ██    ██      ██      ██    ██  ██  ██  ██      ██      ██  ██  ██  ██  ██    ██  ██████
+timeout /NoBreak /t 1 >nul
+echo     ██      ██    ██      ██      ██    ██  ██    ████      ██      ██  ██    ████  ██    ██  ██
+timeout /NoBreak /t 1 >nul
+echo     ██      ████████      ██████  ████████  ██      ██      ██      ██  ██      ██  ████████  ██████
+timeout /NoBreak /t 1 >nul
+
+pause >nul
+cls
+cmd
